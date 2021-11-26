1753e05e7a27362346f84159e63456123d05185e
GianniPio <giannipiopellegrino00@gmail.com>
2021-11-26 15:03:38 +0100

    finished main

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,6 +1,8 @@
 <template>
   <main>
-      
+      <div id="container">
+
+      </div>
   </main>
 </template>
 
@@ -12,5 +14,17 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+    main {
+        background-color: #1e2d3b;
+        height: auto;
+
+        #container {
+            width: 70%;
+            margin: 0 auto;
+            padding: 20px 0;
+            border: 1px solid white;
+        }
+    }
 
 </style>
